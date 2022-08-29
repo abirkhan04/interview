1c6405467bf513fe04032531f6347f791b862d11
abirkhan04 <abirkhan8536@gmail.com>
2022-08-29 20:03:48 +0600

    [] Counter feature is removed.

--- front-end/src/app/store.ts
+++ front-end/src/app/store.ts
@@ -1,11 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import loginReducer from './login/LoginSlice';
 import homeReducer from './home/HomeSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     login: loginReducer,
     home: homeReducer,
   },
